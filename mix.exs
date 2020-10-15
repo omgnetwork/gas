fee2dc94181220986291e12275505dc74f56183f
Ino Murko <ino.murko@outlook.com>
2020-10-15 22:01:06 +0200

    log gas

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Gas.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "1.6.0"},
+      {:httpoison, "~> 1.7.0"},
       {:jason, "~> 1.2"}
     ]
   end
